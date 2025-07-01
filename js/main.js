87a371c97df9e4e52a53754ea0ef94c96474e7cb
BlackhorseAcademy <omar.omarb415@gmail.com>
2025-07-01 17:12:52 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ let loaderShown = localStorage.getItem("loaderShown");
 
       
       localStorage.setItem("loaderShown", "true");
-    }, 1000);
+    }, );
   } else {
   
     document.querySelector(".lod").classList.add("d-none");
